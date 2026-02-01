1788b6823440050a6890535443911a6d636cc8bb
Nipaaaa1 <frostone224@gmail.com>
2026-02-01 17:22:23 +0000

    feat: add entry detail page and add links to other pages

--- app/diary/urls.py
+++ app/diary/urls.py
@@ -5,5 +5,7 @@ from diary import views
 urlpatterns = [
     path("", views.home_view, name="home_view"),
     path("entry/", views.entry_list_view, name="entry_list_view"),
+    path("entry/<int:entry_id>", views.entry_detail_view, name="entry_detail_view"),
+
 
 ]
--- app/diary/views.py
+++ app/diary/views.py
@@ -4,7 +4,7 @@ from diary.models import Home, Diary
 
 def home_view(request):
     home = Home.objects.first()
-    diaries = Diary.objects.order_by("-created")[:2]
+    diaries = Diary.objects.order_by("-created").values("id", "title", "created")[:2]
 
     return render(
         request, 
@@ -16,7 +16,7 @@ def home_view(request):
     )
 
 def entry_list_view(request):
-    diaries = Diary.objects.order_by("-created")
+    diaries = Diary.objects.order_by("-created").values("id", "title", "created")
 
     return render(
         request,
@@ -25,4 +25,16 @@ def entry_list_view(request):
             "diaries": diaries
         }
     )
+
+def entry_detail_view(request, entry_id):
+    diary = Diary.objects.filter(id__exact=entry_id).first()
+
+    return render(
+        request,
+        "diary/entry_detail.html",
+        {
+            "diary": diary
+        }
+    )
+
 
